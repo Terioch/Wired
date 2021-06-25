6195b316e23c48d4ca3d686d0f21fb8d9fea8b11
Rio <riostockton@gmail.com>
2021-06-25 15:04:37 +0100

    Setup initial routes and components and configured firebase and socket client

--- server/index.js
+++ server/index.js
@@ -4,14 +4,25 @@ const socketio = require("socket.io");
 
 const app = express();
 const server = http.createServer(app);
-const io = socketio(server);
 const PORT = process.env.PORT || 5000;
-
-io.on("connection", socket => {
-  console.log("Initialized a new web socket connection...");
-  socket.on("message", (message) => {
-    console.log(message);
-  });
+const io = socketio(server, {
+	cors: {
+		origin: "*",
+		methods: ["GET", "POST"],
+		allowedHeaders: ["tk_token"],
+		credentials: true,
+	},
 });
 
-server.listen(PORT, () => console.log(`Server is listening on port ${PORT}`));
+// Listen for a new web socket connection
+io.on("connection", socket => {
+	console.log("Initialized a new web socket connection...");
+	socket.on("message", message => {
+		console.log(message);
+		socket.emit("message", message);
+	});
+});
+
+server.listen(PORT, () =>
+	console.log(`Server is listening on port ${PORT}`)
+);
