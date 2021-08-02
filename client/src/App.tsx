4d5ac8051d7abad110b4d6bf2271ad5c5579b5e4
Rio <riostockton@gmail.com>
2021-08-02 10:41:41 +0100

    Change: Finalized implementation of authenticated routes

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,50 +1,49 @@
 import React from "react";
-import { Route, Switch, Link, Redirect } from "react-router-dom";
+import { Route, Switch, Redirect, RouteProps } from "react-router-dom";
 import Routes from "./routes/Routes";
-import Components from "./components/Components";
 import { useAuth } from "./contexts/authContext";
-import { makeStyles } from "@material-ui/core";
 
 const { Login, Dashboard, Room } = Routes;
 
-const useStyles = makeStyles(theme => ({}));
-
-function App() {
-	const classes = useStyles();
+const AuthenticatedRoute: React.FC<RouteProps> = ({
+	children,
+	...rest
+}) => {
 	const { isAuthenticated } = useAuth();
 
-	const authenticatedRoutes = <Dashboard />;
+	return (
+		<Route
+			{...rest}
+			render={() =>
+				isAuthenticated() ? children : <Redirect to="/login" />
+			}
+		/>
+	);
+};
+
+function App() {
+	const { isAuthenticated } = useAuth();
 
 	return (
 		<main>
 			<Switch>
+				<AuthenticatedRoute exact path="/">
+					<Dashboard />
+				</AuthenticatedRoute>
 				<Route
 					exact
-					path="/"
+					path="/login"
 					render={() =>
-						isAuthenticated() ? (
-							<Redirect to="/dashboard" />
-						) : (
-							<Redirect to="/login" />
-						)
+						isAuthenticated() ? <Redirect to="/dashboard" /> : <Login />
 					}
 				/>
-				<Route exact path="/login" component={Login} />
-				<Route
-					exact
-					path="/dashboard"
-					render={() =>
-						isAuthenticated() ? <Dashboard /> : <Redirect to="/login" />
-					}
-				/>
-				<Route exact path="/room/:slug" component={Room} />
+				<AuthenticatedRoute exact path="/dashboard">
+					<Dashboard />
+				</AuthenticatedRoute>
+				<AuthenticatedRoute exact path="/room/:slug">
+					<Room />
+				</AuthenticatedRoute>
 			</Switch>
-			<Route exact path="/">
-				<div>
-					<Link to="/dashboard">Go to Dashboard</Link>
-				</div>
-				<Link to="/login">Go to Login</Link>
-			</Route>
 		</main>
 	);
 }
