56a20a6a2678115f4949b21012f2d0540a2c014a
Rio <riostockton@gmail.com>
2021-08-16 12:14:03 +0100

    Configured database connection to operate conditionally based on production or development status

--- server/config/db.js
+++ server/config/db.js
@@ -1,10 +1,19 @@
 const { Pool } = require("pg");
 // const connectionString = `postgressql://${process.env.REACT_APP_DB_USERNAME}:${process.env.REACT_APP_DB_PASSWORD}@localhost:5432/wired`;
 
-module.exports = new Pool({
+const devConfig = {
 	user: process.env.REACT_APP_DB_USERNAME,
 	password: process.env.REACT_APP_DB_PASSWORD,
-	database: "wired",
-	host: "localhost",
-	port: 5432,
-});
+	database: process.env.REACT_APP_DB_NAME,
+	host: process.env.REACT_APP_DB_HOST,
+	port: process.env.REACT_APP_DB_PORT,
+};
+
+const prodConfig = {
+	connectionString: process.env.DATABASE_URL, // Heroku add-on
+};
+
+const dev = process.env.NODE_ENV !== "production";
+const config = dev ? devConfig : prodConfig;
+
+module.exports = new Pool(config);
