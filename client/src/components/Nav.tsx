ce62e5773cf4d25d0f32373af093f0a7f6983254
Rio <riostockton@gmail.com>
2021-08-17 12:49:50 +0100

    Made git case sensitive

---
+++ client/src/components/Nav.tsx
@@ -0,0 +1,60 @@
+import React from "react";
+import { useAuth } from "../contexts/authContext";
+import {
+	Typography,
+	AppBar,
+	Toolbar,
+	Button,
+	makeStyles,
+} from "@material-ui/core";
+
+interface Props {}
+
+const useStyles = makeStyles(theme => ({
+	appbar: {
+		backgroundColor: "#27262C",
+	},
+	toolbar: {
+		display: "flex",
+		justifyContent: "space-between",
+		alignItems: "center",
+		padding: theme.spacing(0, 3),
+		[theme.breakpoints.down("xs")]: {
+			padding: theme.spacing(0, 1.5),
+		},
+	},
+	logo: {},
+	logout: {
+		color: "#ffffff",
+		// backgroundColor: "#1E8F29",
+		// "&:hover": {
+		// 	backgroundColor: "#28992E",
+		// },
+	},
+}));
+
+const Nav: React.FC<Props> = () => {
+	const classes = useStyles();
+	const { logout } = useAuth();
+
+	return (
+		<AppBar className={classes.appbar} position="fixed">
+			<Toolbar className={classes.toolbar}>
+				<Typography className={classes.logo} variant="h4">
+					Wired
+				</Typography>
+				<Button
+					className={classes.logout}
+					variant="contained"
+					color="primary"
+					size="large"
+					onClick={logout}
+				>
+					Logout
+				</Button>
+			</Toolbar>
+		</AppBar>
+	);
+};
+
+export default Nav;
