36326f71397f43eb0a7fafc527a60e30bb19f836
Rio <riostockton@gmail.com>
2021-08-17 12:27:04 +0100

    Amended server url

--- client/src/config/server.ts
+++ client/src/config/server.ts
@@ -2,4 +2,4 @@ const dev = process.env.NODE_ENV !== "production";
 
 export const SERVER_URL = dev
 	? "http://localhost:3000"
-	: process.env.REACT_APP_SERVER_URL;
+	: process.env.REACT_APP_VERCEL_URL;
